5040562104a78e8f9da5bedf5dc56cf581a04dd6
muschellij2 <muschellij2@gmail.com>
2017-10-10 11:24:10 -0400

    Fixing typos.

--- R/subject_id_to_visit_id.R
+++ R/subject_id_to_visit_id.R
@@ -2,7 +2,7 @@
 #' @title Kirby 21 Subject Identifiers to NITRC Visit Identifiers
 #'
 #' @description A dataset containing the mapping from the Subject IDs from the 
-#' Kirby demographics to the KKI2009 idenifiers on NITRC
+#' Kirby demographics to the KKI2009 identifiers on NITRC
 #' @format A data frame with 42 rows and 4 columns 
 #'
 #' @source \url{https://www.nitrc.org/frs/?group_id=313}
